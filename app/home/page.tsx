10ed29b1e2cbdc0d61ec8f4f4cfa216adde09f56
Vivian Phung <dev+github@vivianphung.com>
2023-07-25 14:15:04 -0700

    tenants using nextjs

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -4,18 +4,7 @@ import Image from "next/image";
 export default function HomePage() {
   return (
     <div className="flex h-screen flex-col items-center justify-center space-y-10 bg-black">
-      <form className="flex flex-col items-center justify-center space-y-10">
-        <div className="flex flex-col items-center justify-center space-y-10">
-          <Image
-            src="/images/logo.png"
-            alt="Picture of the author"
-            width={200}
-            height={200}
-          />
-          <h1 className="text-4xl text-white">Welcome to the</h1>
-          <h1 className="text-4xl text-white">Domain Configuration</h1>
-        </div>
-      </form>
+      <h1 className="text-4xl font-bold text-white">Igloo Tools</h1>
     </div>
   );
 }
