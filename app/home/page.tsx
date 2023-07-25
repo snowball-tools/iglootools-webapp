07df5105e0f3ebc06c616755d3e671f99bb425de
Vivian Phung <dev+github@vivianphung.com>
2023-07-25 15:12:52 -0700

    start webauthn

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -1,10 +1,24 @@
-import { InlineSnippet } from "@/components/form/domain-configuration";
-import Image from "next/image";
+"use client";
+
+import { useEffect, useState } from "react";
+import { browserSupportsWebAuthn } from "@simplewebauthn/browser";
+import Home from "@/components/Home";
+import Login from "@/components/Login";
 
 export default function HomePage() {
+  // hardcoded for now
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+  const [isWebAuthnSupported, setIsWebAuthnSupported] = useState(true);
+  useEffect(() => {
+    const supported =
+      browserSupportsWebAuthn() && !navigator.userAgent.includes("Firefox");
+    setIsWebAuthnSupported(supported);
+  }, []);
+
   return (
     <div className="flex h-screen flex-col items-center justify-center space-y-10 bg-black">
       <h1 className="text-4xl font-bold text-white">Igloo Tools</h1>
+      {isAuthenticated ? <Home /> : <Login />}
     </div>
   );
 }
